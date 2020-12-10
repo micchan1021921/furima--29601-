49f75d71fca9b11ee0cffc58dadaed2ae33d0b74
micchan1021921 <micchanyade@yahoo.co.jp>
2020-12-11 00:09:29 +0900

    ユーザー管理機能カラム追加・DBへの保存

--- db/schema.rb
+++ db/schema.rb
@@ -15,6 +15,12 @@ ActiveRecord::Schema.define(version: 2020_12_07_132627) do
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "nickname", default: "", null: false
+    t.string "last_name", default: "", null: false
+    t.string "first_name", default: "", null: false
+    t.string "last_name_kana", default: "", null: false
+    t.string "first_name_kana", default: "", null: false
+    t.date "birth_date", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
