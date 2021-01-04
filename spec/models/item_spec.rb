ddbf61a81eb3ba8806e5dfdc96161fadb5e70a8e
micchan1021921 <micchanyade@yahoo.co.jp>
2021-01-04 14:23:14 +0900

    手数料・利益の計算

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+end
+
+describe '商品出品機能' do
+  context '出品登録がうまくいくとき' do
+    it "image、name、info、category_id、sales_status_id、shippings_tee_status_id、prefecture_id、scheduled_delivary_id、priceが存在すれば登録できる" do
+      expect(@item).to be_valid
+    end
+  end
 end
